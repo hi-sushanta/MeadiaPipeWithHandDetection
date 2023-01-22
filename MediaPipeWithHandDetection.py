244cbf1f8ffcc99b213dd3414d3adf02a40c4997
HIWHY <hiwhy@proton.me>
2023-01-22 09:22:25 +0530

    Any unnecassy print state ment to remove in this file under

--- MediaPipeWithHandDetection.py
+++ MediaPipeWithHandDetection.py
@@ -24,19 +24,12 @@ with mp_hands.Hands(
         # Convert the BGR image to RGB before processing.
         results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-        # Print handedness and draw hand landmarks on the image.
-        print('Handedness:', results.multi_handedness)
         if not results.multi_hand_landmarks:
             continue
         image_height, image_width, _ = image.shape
         annotated_image = image.copy()
         for hand_landmarks in results.multi_hand_landmarks:
-            print('hand_landmarks:', hand_landmarks)
-            print(
-                f'Index finger tip coordinates: (',
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-            )
+        
             mp_drawing.draw_landmarks(
                 annotated_image,
                 hand_landmarks,
@@ -48,7 +41,7 @@ with mp_hands.Hands(
         for hand_world_landmarks in results.multi_hand_world_landmarks:
             mp_drawing.plot_landmarks(
                 hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
-
+#
 
 def callback(image):
     # For webcam input:
